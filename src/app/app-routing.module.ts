6c4c3acee383d65ef648f4f5eb88d43bc8e6e640
EbedGuerra <ebedguerra@gmail.com>
2021-08-27 17:08:03 -0500

    feat: add angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './modules/layout/pages/about/about.component';
+import { HomeComponent } from './modules/layout/pages/home/home.component';
+import { ProgramComponent } from './modules/layout/pages/program/program.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nosotros', component: AboutComponent },
+  { path: 'programas', component: ProgramComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ProgramComponent } from './pages/program/program.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, MainComponent, HomeComponent, AboutComponent, ProgramComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  exports:[
+    MainComponent
+  ]
+})
+export class LayoutModule { }
